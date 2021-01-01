2003b7a180e28be128655850af55b3f6262a2188
Andrew <mrbondmustdie@gmail.com>
2021-01-02 00:03:11 +0800

    Most api routes now working

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,29 @@
 const router = require("express").Router();
 const Workout = require("../models/workouts.js");
 
-// router.post("/api/transaction", ({ body }, res) => {
-//   Transaction.create(body)
-//     .then(dbTransaction => {
-//       res.json(dbTransaction);
-//     })
-//     .catch(err => {
-//       res.status(400).json(err);
-//     });
-// });
+router.post("/workouts", ({ body }, res) => {
+    Workout.create(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.put("/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(
+        params.id,
+
+        { $push: { exercises: body } },
+    )
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        })
+        .catch((error) => {
+            res.json(error);
+        });
+});
 
 // router.post("/api/transaction/bulk", ({ body }, res) => {
 //   Transaction.insertMany(body)
@@ -21,11 +35,22 @@ const Workout = require("../models/workouts.js");
 //     });
 // });
 
+router.get("/workouts/range", (req, res) => {
+    Workout.find({})
+        .sort({ date: -1 })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
 router.get("/workouts", (req, res) => {
     Workout.find({})
         .sort({ date: -1 })
-        .then(dbTransaction => {
-            res.json(dbTransaction);
+        .then(dbWorkout => {
+            res.json(dbWorkout);
         })
         .catch(err => {
             res.status(400).json(err);
